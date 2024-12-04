d29e0be0fd6b2ce26c6bda95f16224d1abd28346
sudo <3577669408@qq.com>
2024-12-05 02:09:33 +0800

    [build]: build on server successfully

--- config/puma.rb
+++ config/puma.rb
@@ -24,7 +24,9 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# config/puma.rb
+port ENV.fetch("PORT") { 8081 }
+bind "tcp://0.0.0.0:8081"
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
